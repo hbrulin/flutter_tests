a9f4a715ab166e8d6f100ce44d0cc48abb55a93e
babaoriley <helenebrulin@live.fr>
2020-11-05 17:35:04 +0100

    backdrop everywhere

--- cresus_app/lib/feelinguseful.dart
+++ cresus_app/lib/feelinguseful.dart
@@ -7,11 +7,29 @@ class FeelingUseful extends StatefulWidget{
 }
 
 class _FeelingUsefulState extends State<FeelingUseful> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-        'lala'
+    return BackdropScaffold(
+      title: Text('Feeling Useful'),
+      iconPosition: BackdropIconPosition.leading,
+      actions: [
+        BackdropToggleButton(
+          icon: AnimatedIcons.list_view,
+        ),
+      ],
+      frontLayer: Text('Lala'),
+      backLayer: BackdropNavigationBackLayer(
+        items: [
+          ListTile(title: Text("Mon profil", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+          ListTile(title: Text("Mon épargne", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+          ListTile(title: Text("Mes contrats", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+          ListTile(title: Text("Paramètres", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+          ListTile(title: Text("Legal stuff", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+        ],
+        onTap: (int position) => {setState(() => _currentIndex = position)},
+      ),
     );
   }
 }
--- cresus_app/lib/products.dart
+++ cresus_app/lib/products.dart
@@ -7,11 +7,29 @@ class Products extends StatefulWidget{
 }
 
 class _ProductsState extends State<Products> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-        'lala'
+    return BackdropScaffold(
+      title: Text('Produits'),
+      iconPosition: BackdropIconPosition.leading,
+      actions: [
+        BackdropToggleButton(
+          icon: AnimatedIcons.list_view,
+        ),
+      ],
+      frontLayer: Text('Lala'),
+      backLayer: BackdropNavigationBackLayer(
+        items: [
+          ListTile(title: Text("Mon profil", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+          ListTile(title: Text("Mon épargne", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+          ListTile(title: Text("Mes contrats", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+          ListTile(title: Text("Paramètres", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+          ListTile(title: Text("Legal stuff", style: TextStyle(fontSize: 20, color: Colors.black.withOpacity(0.6)))),
+        ],
+        onTap: (int position) => {setState(() => _currentIndex = position)},
+      ),
     );
   }
 }
